56ce1386e69034f2a5810faf8859dc6ab40827ae
Adnan Sattikar <adnan.sattikar2001@gmail.com>
2023-09-23 21:21:44 +0530

    text to speech done

--- speech-to-text/src/App.jsx
+++ speech-to-text/src/App.jsx
@@ -3,14 +3,23 @@ import './App.css';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import useClipboard from 'react-use-clipboard'
 import { useState } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
+
 function App() {
   const startListening = () =>SpeechRecognition.startListening({continuous: true,language:'en-IN'});
-  const {transcript, browserSupportsSpeechRecognition} = useSpeechRecognition();
+  const {transcript, resetTranscript, browserSupportsSpeechRecognition} = useSpeechRecognition();
   const [copiedText, setCopiedText] = useState();
 
   const [isCopied, setCopied] = useClipboard(copiedText)
+  
+  const [text,setText] = useState();
+  const {speak} = useSpeechSynthesis()
+  
   if(!browserSupportsSpeechRecognition){
     return null;
+  }
+  const startSpeaking = ()=>{
+    speak({text:text})
   }
   return (
     <div className="container">
@@ -23,6 +32,13 @@ function App() {
         <button className="btn btn-warning" onClick={setCopied}>{isCopied ? 'Copied':'Copy to Clipboard'}</button>
         <button className="btn btn-success" onClick={startListening}>Listen</button>
         <button className="btn btn-danger" onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button className="btn btn-dark" onClick={resetTranscript}>Clear</button>
+      </div>
+
+      <h2>Text to Speech Converter</h2>
+      <textarea className='content-box' onChange={(e)=>{setText(e.target.value)}}></textarea>
+      <div className="btn-style">
+        <button className="btn btn-info" onClick={startSpeaking}>Speak</button>
       </div>
     </div>
   );
